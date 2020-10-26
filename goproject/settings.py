1fcb9e4583c5d1b4bc02e1ae2a38be99780fd998
Jayce0918 <51176006+Jayce0918@users.noreply.github.com>
2020-10-26 13:03:00 +0800

    輪播控制JS

--- goproject/settings.py
+++ goproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3ll^j1un@agui&_4uy)9r8e4u#6apg0v&-b)(%rhph-rc=7-2i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost","127.0.0.1"]
 
